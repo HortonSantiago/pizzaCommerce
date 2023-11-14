992c7616f10efe543e0047ae0bf02f78759a275e
Saniago Horton <santiagohorton@gmail.com>
2023-11-14 20:12:56 -0300

    chekoutFormik

--- src/components/pages/checkOutFormik/CheckoutFormik.jsx
+++ src/components/pages/checkOutFormik/CheckoutFormik.jsx
@@ -1,44 +1,63 @@
 import { Button, TextField } from "@mui/material";
 import { useFormik } from "formik";
 import * as Yup from "yup";
+import Swal from "sweetalert2";
 
 export const CheckOutFormik = () => {
-  const { handleChange, handleSubmit, errors, touched } = useFormik({
-    initialValues: {
-      primerNombre: "",
-      segundoNombre: "",
-      email: "",
-      password: "",
-      repeatPassword: "",
-    },
-    onSubmit: (data) => {
-      console.log("se envió");
-      console.log(data);
-    },
-    validateOnChange: false,
-    validationSchema: Yup.object({
-      primerNombre: Yup.string()
-        .required("El nombre es obligatorio")
-        .min(5, "El nombre debe tener al menos 5 caracteres")
-        .max(20, "El nombre no puede tener más de 20 caracteres"),
-      segundoNombre: Yup.string()
-        .required("El apellido es obligatorio")
-        .min(5, "El apellido debe tener al menos 5 caracteres")
-        .max(20, "El apellido no puede tener más de 20 caracteres"),
-      email: Yup.string()
-        .email("Ingrese un email válido")
-        .required("El email es obligatorio"),
-      password: Yup.string()
-        .required("La contraseña es obligatoria")
-        .matches(/^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{6,15}$/, {
-          message:
-            "La contraseña debe tener al menos una mayúscula y un carácter especial",
-        }),
-      repeatPassword: Yup.string()
-        .required("La repetición de la contraseña es obligatoria")
-        .oneOf([Yup.ref("password")], "Las contraseñas no coinciden"),
-    }),
-  });
+  const { handleChange, handleSubmit, errors, touched, isValid, isSubmitting } =
+    useFormik({
+      initialValues: {
+        primerNombre: "",
+        segundoNombre: "",
+        email: "",
+        telefono: "",
+        password: "",
+        repeatPassword: "",
+      },
+      onSubmit: async () => {
+        try {
+          const orderId = await saveOrderToDatabase();
+
+          // Show SweetAlert success
+          Swal.fire({
+            icon: "success",
+            title: "Compra realizada con éxito",
+            text: `Número de orden: ${orderId}`,
+          });
+
+          console.log(
+            "Orden guardada en la base de datos. Número de orden:",
+            orderId
+          );
+        } catch (error) {
+          console.error("Error al guardar la orden:", error);
+        }
+      },
+      validateOnChange: false,
+      validationSchema: Yup.object({
+        primerNombre: Yup.string()
+          .required("El nombre es obligatorio")
+          .min(5, "El nombre debe tener al menos 5 caracteres")
+          .max(20, "El nombre no puede tener más de 20 caracteres"),
+        segundoNombre: Yup.string()
+          .required("El apellido es obligatorio")
+          .min(5, "El apellido debe tener al menos 5 caracteres")
+          .max(20, "El apellido no puede tener más de 20 caracteres"),
+        email: Yup.string()
+          .email("Ingrese un email válido")
+          .required("El email es obligatorio"),
+        telefono: Yup.string().required("El teléfono es obligatorio"),
+        password: Yup.string()
+          .required("La contraseña es obligatoria")
+          .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,15}$/, {
+            message:
+              "La contraseña debe tener al menos una mayúscula y ser alfanumérica",
+          }),
+        repeatPassword: Yup.string()
+          .required("La repetición de la contraseña es obligatoria")
+          .oneOf([Yup.ref("password")], "Las contraseñas no coinciden"),
+      }),
+    });
 
   const getInputProps = (fieldName, label) => ({
     id: `filled-${fieldName}`,
@@ -50,8 +69,8 @@ export const CheckOutFormik = () => {
     helperText: touched[fieldName] ? errors[fieldName] : "",
     style: {
       borderColor: touched[fieldName] && errors[fieldName] ? "red" : "",
-      marginBottom: "15px", // Espaciado uniforme entre los campos
-      backgroundColor: "white", // Fondo blanco para todos los campos
+      marginBottom: "15px",
+      backgroundColor: "white",
     },
   });
 
@@ -64,11 +83,17 @@ export const CheckOutFormik = () => {
         <TextField {...getInputProps("primerNombre", "Nombre")} />
         <TextField {...getInputProps("segundoNombre", "Apellido")} />
         <TextField {...getInputProps("email", "Email")} />
+        <TextField {...getInputProps("telefono", "Teléfono")} />
         <TextField {...getInputProps("password", "Contraseña")} />
         <TextField {...getInputProps("repeatPassword", "Repetir Contraseña")} />
 
-        <Button variant="contained" type="submit">
-          Enviar
+        {/* 'Realizar Compra' button is disabled only during form submission */}
+        <Button
+          variant="contained"
+          type="submit"
+          disabled={!isValid || isSubmitting}
+        >
+          Realizar Compra
         </Button>
         <Button variant="contained" type="button">
           Cancelar
@@ -77,3 +102,11 @@ export const CheckOutFormik = () => {
     </div>
   );
 };
+
+async function saveOrderToDatabase() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("12345");
+    }, 1000);
+  });
+}
