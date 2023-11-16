6f03a83c270207e7e5d1548bd07d41a04bb4644a
Saniago Horton <santiagohorton@gmail.com>
2023-11-15 21:07:49 -0300

    Mensaje del commit

--- src/components/pages/checkOutFormik/CheckoutFormik.jsx
+++ src/components/pages/checkOutFormik/CheckoutFormik.jsx
@@ -1,23 +1,16 @@
 import { Button, TextField, Typography } from "@mui/material";
 import { useFormik } from "formik";
+import Link from "@mui/material";
 import * as Yup from "yup";
 import Swal from "sweetalert2";
-<<<<<<< HEAD
 
 export const CheckOutFormik = () => {
   const { handleChange, handleSubmit, errors, touched, isValid, isSubmitting } =
-=======
-import { Link } from "react-router-dom"; // Importa Link
-
-export const CheckOutFormik = () => {
-  const { handleChange, handleSubmit, errors, touched, isValid, dirty } =
->>>>>>> ramaB
     useFormik({
       initialValues: {
         primerNombre: "",
         segundoNombre: "",
         email: "",
-<<<<<<< HEAD
         telefono: "",
         password: "",
         repeatPassword: "",
@@ -40,50 +33,26 @@ export const CheckOutFormik = () => {
         } catch (error) {
           console.error("Error al guardar la orden:", error);
         }
-=======
-        password: "",
-        repeatPassword: "",
-      },
-      onSubmit: (data) => {
-        console.log("Se envió el formulario");
-        console.log(data);
->>>>>>> ramaB
       },
       validateOnChange: false,
       validationSchema: Yup.object({
         primerNombre: Yup.string()
           .required("El nombre es obligatorio")
-<<<<<<< HEAD
           .min(5, "El nombre debe tener al menos 5 caracteres")
           .max(20, "El nombre no puede tener más de 20 caracteres"),
         segundoNombre: Yup.string()
           .required("El apellido es obligatorio")
           .min(5, "El apellido debe tener al menos 5 caracteres")
-=======
-          .min(2, "El nombre debe tener al menos 2 caracteres")
-          .max(20, "El nombre no puede tener más de 20 caracteres"),
-        segundoNombre: Yup.string()
-          .required("El apellido es obligatorio")
-          .min(2, "El apellido debe tener al menos 2 caracteres")
->>>>>>> ramaB
           .max(20, "El apellido no puede tener más de 20 caracteres"),
         email: Yup.string()
           .email("Ingrese un email válido")
           .required("El email es obligatorio"),
-<<<<<<< HEAD
         telefono: Yup.string().required("El teléfono es obligatorio"),
         password: Yup.string()
           .required("La contraseña es obligatoria")
           .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,15}$/, {
             message:
               "La contraseña debe tener al menos una mayúscula y ser alfanumérica",
-=======
-        password: Yup.string()
-          .required("La contraseña es obligatoria")
-          .matches(/^(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.*[a-z]).{6,15}$/, {
-            message:
-              "La contraseña debe tener al menos una mayúscula y un carácter especial",
->>>>>>> ramaB
           }),
         repeatPassword: Yup.string()
           .required("La repetición de la contraseña es obligatoria")
@@ -101,10 +70,7 @@ export const CheckOutFormik = () => {
     error: touched[fieldName] && Boolean(errors[fieldName]),
     helperText: touched[fieldName] ? errors[fieldName] : "",
     style: {
-<<<<<<< HEAD
       borderColor: touched[fieldName] && errors[fieldName] ? "red" : "",
-=======
->>>>>>> ramaB
       marginBottom: "15px",
       backgroundColor: "white",
     },
@@ -153,19 +119,14 @@ export const CheckOutFormik = () => {
       >
         <TextField {...getInputProps("primerNombre", "Nombre")} />
         <TextField {...getInputProps("segundoNombre", "Apellido")} />
-<<<<<<< HEAD
         <TextField {...getInputProps("email", "Email")} />
         <TextField {...getInputProps("telefono", "Teléfono")} />
-=======
-        <TextField {...getInputProps("email", "Email")} type="email" />
->>>>>>> ramaB
         <TextField {...getInputProps("password", "Contraseña")} />
         <TextField
           {...getInputProps("repeatPassword", "Repetir Contraseña")}
           type="password"
         />
 
-<<<<<<< HEAD
         {/* 'Realizar Compra' button is disabled only during form submission */}
         <Button
           variant="contained"
@@ -176,19 +137,6 @@ export const CheckOutFormik = () => {
         </Button>
         <Button variant="contained" type="button">
           Cancelar
-=======
-        <Button
-          variant="contained"
-          type="submit"
-          style={{
-            backgroundColor: "#FF4500",
-            color: "white",
-            marginTop: "20px",
-          }}
-          disabled={!dirty || !isValid}
-        >
-          Enviar Pedido
->>>>>>> ramaB
         </Button>
         <Link to="/cart">
           <Button
