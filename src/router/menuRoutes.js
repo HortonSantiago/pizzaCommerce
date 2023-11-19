5b7cb890ff60095045ddafc82cece709af1b011a
Saniago Horton <santiagohorton@gmail.com>
2023-11-18 21:35:30 -0300

    suge

--- src/router/menuRoutes.js
+++ src/router/menuRoutes.js
@@ -1,19 +1,18 @@
 import Cart from "../components/pages/Cart/Cart.jsx";
 import ItemDetailContainer from "../components/pages/ItemDetailContainer/ItemDetailContainer.jsx";
-
-import ItemListContainer from "../components/pages/ItemListContainer/ItemListContainer.jsx";
+import CustomItemListContainer from "../components/pages/ItemListContainer/ItemListContainer.jsx";
 import { CheckOutFormik } from "../components/pages/checkOutFormik/CheckoutFormik.jsx";
 
 export const routes = [
   {
     id: "home",
     path: "/",
-    Element: ItemListContainer,
+    Element: CustomItemListContainer,
   },
   {
     id: "category",
     path: "/category/:categoryName",
-    Element: ItemListContainer,
+    Element: CustomItemListContainer,
   },
   {
     id: "cart",
