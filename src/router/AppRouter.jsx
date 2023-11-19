a979cd738cac0278481e89e34fc2b15947c49145
Saniago Horton <santiagohorton@gmail.com>
2023-11-18 21:59:50 -0300

    k

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,21 +1,18 @@
-import { ThemeProvider } from "@emotion/react";
 import { Route, Routes } from "react-router-dom";
 import Layout from "../components/layout/footer/Layout";
-import { CustomTheme } from "../components/themeConfig";
+
 import { routes } from "./menuRoutes";
 
 const AppRouter = () => {
   return (
-    <ThemeProvider theme={CustomTheme}>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          {routes.map(({ id, path, Element }) => (
-            <Route key={id} index path={path} element={<Element />} />
-          ))}
-        </Route>
-        <Route path="*" element={<h1>NOT FOUND</h1>} />
-      </Routes>
-    </ThemeProvider>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {routes.map(({ id, path, Element }) => (
+          <Route key={id} index path={path} element={<Element />} />
+        ))}
+      </Route>
+      <Route path="*" element={<h1>NOT FOUND</h1>} />
+    </Routes>
   );
 };
 
