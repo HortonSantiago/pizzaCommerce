ee907525cb5af789c785ee832a8f0b2f48317a3c
Saniago Horton <santiagohorton@gmail.com>
2023-11-20 11:42:06 -0300

    p

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,6 +1,6 @@
-import Footer from "./Footer/Footer";
+import Footer from "./footer/Footer";
 import { Outlet } from "react-router-dom";
-import ResponsiveAppBar from "./Navbar/Navbar";
+import ResponsiveAppBar from "./navbar/Navbar";
 
 const Layout = () => {
   return (
